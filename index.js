834ab2f446b0f5fee70610cc99eab5f885b7f253
yasar budulgan <yb.budulgan@gmail.com>
2019-12-26 22:45:07 +0300

    Add header text

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 const shell = require('shell');
 
+prettierConfig = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  useTabs: false,
+  printWidth: 100,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+};
+
+const init = async () => {
+  clear();
+  console.log(
+    chalk.green(
+      figlet.textSync('PrTfY', {
+        horizontalLayout: 'full',
+      })
+    )
+  );
+}
+
 const main = async () => {
   // show prtfy introduction
   // install GitHook
