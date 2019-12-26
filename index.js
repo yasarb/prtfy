6d4e20430bebb1131f8c564268a42e39e1c9b681
yasar budulgan <yb.budulgan@gmail.com>
2019-12-26 22:41:25 +0300

    Add initial project files

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const figlet = require('figlet');
+const shell = require('shell');
+
+const main = async () => {
+  // show prtfy introduction
+  // install GitHook
+  // ask questions
+  // create the files
+  // configures pre-commit hook
+  // show success message
+}
+
+main();
